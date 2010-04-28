7a3454bbacab5a8b906896c4f9fd9c6f47fd9184
Eivind Barstad Waaler <eivindw@gmail.com>
2010-04-28 15:40:18 +0200

    Adjustments to chapter 3.

--- examples/by-name-param/ByName.scala
+++ examples/by-name-param/ByName.scala
@@ -1,11 +1,14 @@
+// Class implementing function with a by-name body parameter
 class DoIntTimes(val n: Int) {
   def times(body: => Unit) {
     for(i <- 0 until n) body
   }
 }
 
+// Implicit conversion from integers to the class defined above
 implicit def intToDoIntTimes(n: Int) = new DoIntTimes(n)
 
+// Example of use
 2 times {
   println("printed twice!")
 }
